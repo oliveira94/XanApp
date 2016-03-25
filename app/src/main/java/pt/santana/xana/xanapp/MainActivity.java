ded06a43b58cb23354bd14c8742e645d1af7dc18
Nuno Vasconcelos <nunorogerio12@hotmail.com>
2016-03-25 00:28:45 +0000

    Versão 1.1

--- app/src/main/java/pt/santana/xana/xanapp/MainActivity.java
+++ app/src/main/java/pt/santana/xana/xanapp/MainActivity.java
@@ -1,9 +1,13 @@
 package pt.santana.xana.xanapp;
 
+import android.graphics.PixelFormat;
 import android.media.MediaPlayer;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.VideoView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,16 +16,42 @@ public class MainActivity extends AppCompatActivity {
     MediaPlayer mySound3;
     MediaPlayer mySound4;
     MediaPlayer mySound5;
+    VideoView botaoPlay;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mySound1 = MediaPlayer.create(this, R.raw.Desculpah);
-        mySound2 = MediaPlayer.create(this, R.raw.Paxourrah);
-        mySound3 = MediaPlayer.create(this, R.raw.Enfeim);
-        mySound4 = MediaPlayer.create(this, R.raw.Bora_po_omegle);
-        mySound5 = MediaPlayer.create(this, R.raw.Cunouje);
+        mySound1 = MediaPlayer.create(this, R.raw.desculpah);
+        mySound2 = MediaPlayer.create(this, R.raw.paxourrah);
+        mySound3 = MediaPlayer.create(this, R.raw.enfeim);
+        mySound4 = MediaPlayer.create(this, R.raw.borapoomegle);
+        mySound5 = MediaPlayer.create(this, R.raw.cunouje);
+        Button buttonPlayVideo2 = (Button)findViewById(R.id.cabrabutton);
+        VideoView mVideoView2 = (VideoView)findViewById(R.id.cabraVideoView);
+
+        /**
+        got from http://android-coffee.com/tutorial-how-to-play-video-in-android-studio-1-4/
+         **/
+        getWindow().setFormat(PixelFormat.UNKNOWN);
+        //displays a video file
+        String uriPath2 = "android.resource://pt.santana.xana.xanapp/"+R.raw.ahahcabra;
+        Uri uri2 = Uri.parse(uriPath2);
+        mVideoView2.setVideoURI(uri2);
+        mVideoView2.requestFocus();
+        mVideoView2.start();
+
+        //Play again button
+        buttonPlayVideo2.setOnClickListener(new Button.OnClickListener() {
+            public void onClick(View v) {
+                VideoView mVideoView2 = (VideoView) findViewById(R.id.cabraVideoView);
+                String uriPath = "android.resource://pt.santana.xana.xanapp/" + R.raw.ahahcabra;
+                Uri uri2 = Uri.parse(uriPath);
+                mVideoView2.setVideoURI(uri2);
+                mVideoView2.requestFocus();
+                mVideoView2.start();
+            }
+        });
     }
 
     public void PlayMusicDesculpah(View view) {
@@ -43,4 +73,6 @@ public class MainActivity extends AppCompatActivity {
     public void PlayMusicCuNouje(View view) {
         mySound5.start();
     }
+
+
 }
